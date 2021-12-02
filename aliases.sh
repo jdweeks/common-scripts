f25fc292ea28a06a00d2d5b30a23362537b5db44
jdweeks <jdweeks9@gmail.com>
2021-12-02 15:50:21 -0500

    add aliases

---
+++ aliases.sh
@@ -0,0 +1,32 @@
+# add like this:
+# source ./aliases.sh
+
+alias k=kubectl
+alias kg='kubectl get'
+alias kl='kubectl logs'
+
+alias ktx=kubectx
+alias kns=kubens
+
+alias d=docker
+alias dc=docker-compose
+alias dcu='docker-compose up'
+alias dcd='docker-compose down'
+
+alias g=git
+alias gs='git status'
+alias ga='git add -A'
+alias gc='git commit -m'
+alias gpu='git pull'
+alias gps='git push'
+
+alias m=make
+alias mc='make clean'
+alias mi='make install'
+alias mt='make test'
+
+alias tf='terraform'
+alias tfi='terraform init'
+alias tff='terraform fmt'
+alias tfv='terraform validate'
+alias tfp='terraform plan'
